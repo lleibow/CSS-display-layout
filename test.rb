5a782df6c918871e511901236850799ee49c5a32
LAURA LEIBOW <lauraleibow@gmail.com>
2017-01-16 13:11:45 -0500

    syncing

---
+++ test.rb
@@ -0,0 +1,16 @@
+# if (condition is true)
+# work to do when true
+#else
+# workt o do when fa;se
+#end
+
+count =9
+
+if count > 10
+puts "The count is greater than 10"
+elsif count < 10
+  puts "the count is les than ten"
+else
+  #if the condition is false
+  puts "the count is equal to 10"
+end
